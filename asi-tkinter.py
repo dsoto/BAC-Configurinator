0b0b37b6d7e1064869b2f53c50fb12835efa5cfb
Daniel Soto <sotod@sonoma.edu>
2020-09-22 20:37:04 -0700

    mocks up connect menu and adds new row

--- asi-tkinter.py
+++ asi-tkinter.py
@@ -8,14 +8,14 @@ class row(tk.Frame):
 
         tk.Frame.__init__(self, parent)
 
-        self.address_entry = tk.Entry(self)
+        self.address_entry = tk.Entry(self, width=3)
         self.address_entry.insert(tk.END, '259') #command get description, set member variable
         self.address_entry.pack(side=tk.LEFT)
 
-        self.description_label = tk.Label(self, text='default description', width=10)
+        self.description_label = tk.Label(self, text='default description', width=30)
         self.description_label.pack(side = tk.LEFT)
 
-        self.value_entry = tk.Entry(self)
+        self.value_entry = tk.Entry(self, width=6)
         self.value_entry.insert(tk.END, '1000')
         self.value_entry.pack(side=tk.LEFT)
 
@@ -35,19 +35,37 @@ class row(tk.Frame):
 
 
 
-class ExampleApp(tk.Tk):
+class Main_Window(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
 
-        # read button callback needs address
-        # write button callback needs address and value
-        # how to put a frame in the row class and add new instances of the class to the main window
+        # put this in a frame at top
+        # make drop down of serial ports
+        # make button to connect
+        serial_frame = tk.Frame(self)
+        ports = {'one', 'two', 'three'}
+        choice = tk.StringVar()
+        serial_menu = tk.OptionMenu(serial_frame, choice, *ports)
+        serial_menu.pack(side=tk.LEFT)
+        serial_connect_button = tk.Button(serial_frame, text='Connect')
+        serial_connect_button.pack(side=tk.RIGHT)
+        serial_frame.pack()
+
+        # put in a button that inserts a new frame
+        new_frame_button = tk.Button(self, text='New Row', command=self.new_frame)
+        new_frame_button.pack()
 
         frame_1 = row(self)
         frame_1.pack()
 
         frame_2 = row(self)
         frame_2.pack()
+
+
+    def new_frame(self):
+        frame = row(self)
+        frame.pack(side=tk.TOP)
+
 
 
     def read_BAC(self):
@@ -70,7 +88,7 @@ if __name__ == "__main__":
     num_registers = 1  # number of 16-bit readings to make
     device_ID = 0x01   # identifier for the ASI controller to distinguish from other devices
 
-    app = ExampleApp()
+    app = Main_Window()
     app.title('ASI Configurator')
     app.geometry("600x300+300+300")
     app.mainloop()
